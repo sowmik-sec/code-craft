618dca0a4c82336e299424335f47ed404b1f4db1
sowmik-sec <sowmik.sec@gmail.com>
2024-06-26 11:25:46 +0600

    create cart template

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,19 @@
+function Cart() {
+  return (
+    <div className="md:w-2/12 p-6 space-y-5">
+      <h4 className="text-2xl font-bold text-cyan-400">
+        Credit Hour Remaining
+      </h4>
+      <hr />
+      <div>
+        <h5 className="text-xl font-bold">Course Name</h5>
+      </div>
+      <hr />
+      <p>Total Credit Hour: </p>
+      <hr />
+      <h5>Total Price: </h5>
+    </div>
+  );
+}
+
+export default Cart;
