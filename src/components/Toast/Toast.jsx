34192cd033d786ec51342da7d9b0609da141a911
sowmik-sec <sowmik.sec@gmail.com>
2024-06-26 12:02:11 +0600

    create a toast

---
+++ src/components/Toast/Toast.jsx
@@ -0,0 +1,11 @@
+function Toast() {
+  return (
+    <div className="toast toast-center toast-middle">
+      <div className="alert alert-info">
+        <span>Your credit hour has exceeded </span>
+      </div>
+    </div>
+  );
+}
+
+export default Toast;
