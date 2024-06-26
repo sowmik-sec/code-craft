43e3dd8b2ce45815bf5596093afdfbe7a9ee290d
sowmik-sec <sowmik.sec@gmail.com>
2024-06-26 12:52:11 +0600

    show course details length conditionally

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
+
 function Course({ course, handleSelectCourse }) {
+  const [showDetails, setShowDetails] = useState(false);
   const { course_name, cover_img, credit, price, details } = course;
+  const handleDetails = (state) => {
+    setShowDetails(state);
+  };
   return (
     <div className="p-4 space-y-5">
       <img className="w-full h-80 rounded-lg" src={cover_img} alt="" />
       <h3 className="text-lg font-semibold">{course_name}</h3>
       <p>
-        {details.slice(0, 40)}...
-        <span className="cursor-pointer text-violet-700">See Details</span>
+        {!showDetails ? (
+          <div>
+            {details.slice(0, 30)}...
+            <span
+              className="cursor-pointer text-violet-600"
+              onClick={() => handleDetails(true)}
+            >
+              See Details
+            </span>
+          </div>
+        ) : (
+          <div>
+            {details}{" "}
+            <span
+              className="cursor-pointer text-violet-600"
+              onClick={() => handleDetails(false)}
+            >
+              See Less
+            </span>
+          </div>
+        )}
       </p>
       <div className="flex justify-between">
         <p>$ Price: ${price}</p>
