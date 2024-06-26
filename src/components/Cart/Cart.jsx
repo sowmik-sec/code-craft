1e520a9b2dcc37abf87d10605fb2aed839b91cc7
sowmik-sec <sowmik.sec@gmail.com>
2024-06-26 12:36:06 +0600

    complete the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,24 @@
-function Cart() {
+function Cart({ cartObj }) {
+  const { selectedCourses, creditHourRemaining, totalCreditHour, totalPrice } =
+    cartObj;
   return (
     <div className="md:w-2/12 p-6 space-y-5">
       <h4 className="text-2xl font-bold text-cyan-400">
-        Credit Hour Remaining
+        Credit Hour Remaining {creditHourRemaining} hr
       </h4>
       <hr />
       <div>
         <h5 className="text-xl font-bold">Course Name</h5>
+        <ol className="list-decimal">
+          {selectedCourses.map((course, i) => (
+            <li key={i}>{course}</li>
+          ))}
+        </ol>
       </div>
       <hr />
-      <p>Total Credit Hour: </p>
+      <p>Total Credit Hour: {totalCreditHour}</p>
       <hr />
-      <h5>Total Price: </h5>
+      <h5>Total Price: {totalPrice} USD</h5>
     </div>
   );
 }
